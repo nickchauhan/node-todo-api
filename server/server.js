b790ccb7ebf4343a3d5ece89f5c29af9f464a079
Nikhil Chauhan <nikhil.chauhan@realtyredefined.com>
2018-05-28 12:43:43 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,17 @@ app.post("/todos", (req, res) => {
   );
 });
 
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
 app.listen("3000", () => {
   console.log("Server up at Port 3000");
 });
@@ -31,4 +42,3 @@ app.listen("3000", () => {
 module.exports = {
   app
 };
-
