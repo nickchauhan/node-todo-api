dec5007d618b6aeba7b30917f39083175c71c744
Nikhil Chauhan <nikhil.chauhan@realtyredefined.com>
2018-05-28 16:33:17 +0530

    Test For GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,11 +4,23 @@ const request = require("supertest");
 const { app } = require("./../server");
 const { Todo } = require("./../models/todo");
 
+const todos = [
+  {
+    task: "Learn Angular 6"
+  },
+  { task: "Learn Reack 16" }
+];
+
 beforeEach(done => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => done());
 });
 
-describe("Post /todos", () => {
+// Test for POST for /todos
+describe("POST /todos", () => {
   it("should create a new todo", done => {
     var task = "Test the Todo Post api";
 
@@ -24,7 +36,7 @@ describe("Post /todos", () => {
           return done(err);
         }
 
-        Todo.find().then(todos => {
+        Todo.find({ task }).then(todos => {
           expect(todos.length).toBe(1);
           expect(todos[0].task).toBe(task);
           done();
@@ -43,9 +55,22 @@ describe("Post /todos", () => {
         }
 
         Todo.find().then(todos => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         });
       });
+  });
+});
+
+// Test For GET /todos
+describe("GET /todos", () => {
+  it("should get all the todos", done => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
