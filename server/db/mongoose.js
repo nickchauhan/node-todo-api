2ffe5cdabe892768a25c133b33e156c0d52333c9
Nikhil Chauhan <nikhil.chauhan@realtyredefined.com>
2018-05-29 13:14:16 +0530

    Mongo Db Prod and dev

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 // mongoose.connect("mongodb://localhost:27017/TodoApp");
 // mongodb://<dbuser>:<dbpassword>@ds137600.mlab.com:37600/todoapp
+var monogoDbURL;
+if (process.env.NODE_ENV) {
+  monogoDbURL = "mongodb://nikhil:nickMongo18@ds137600.mlab.com:37600/todoapp";
+} else {
+  monogoDbURL = "mongodb://localhost:27017/TodoApp";
+}
 
-mongoose.connect(
-  "mongodb://nikhil:nickMongo18@ds137600.mlab.com:37600/todoapp" ||
-    "mongodb://localhost:27017/TodoApp"
-);
+mongoose.connect(monogoDbURL);
 
 module.exports = {
   mongoose
