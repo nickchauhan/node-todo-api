49197887db38d44363dccf4ccf0e4defe9d55691
Nikhil Chauhan <nikhil.chauhan@realtyredefined.com>
2018-05-31 20:04:26 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,10 +1,26 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-var data = {
-  id: 10
-};
+// var data = {
+//   id: 10
+// };
 
-var token = jwt.sign(data, "nickhash");
-console.log(token);
-var decoded = jwt.verify(token, "nickhash");
-console.log(decoded);
+// var token = jwt.sign(data, "nickhash");
+// console.log(token);
+// var decoded = jwt.verify(token, "nickhash");
+// console.log(decoded);
+
+const bcrypt = require("bcryptjs");
+var pass = "nick@password";
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(pass, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPass = '$2a$10$EZ3m8YddL8Gufoh6/ORXVOnDNp5MyeF9v6Q8hg0J4nGFByqLGNx7.';
+
+
+bcrypt.compare(pass, hashedPass, (err, res) => {
+  console.log(res);
+});
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const validator = require("validator");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 // Schema for Users
 var UserSchema = new mongoose.Schema({
@@ -55,6 +56,21 @@ UserSchema.static("findByToken", function(token) {
     "tokens.token": token,
     "tokens.access": decoded.access
   });
+});
+
+//To hash the password
+UserSchema.pre("save", function(next) {
+  var user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 // Add methods (are used on user instances) to the User Model
