934a284ffb85ce6b4c0cde8a2da64a21afbb1069
Nikhil Chauhan <nikhil.chauhan@realtyredefined.com>
2018-06-03 15:27:58 +0530

    Remove the Old Config

--- server/config/config.js
+++ server/config/config.js
@@ -8,15 +8,3 @@ if (env === "development" || env === "test") {
     process.env[key] = envConfig[key];
   });
 }
-
-// if (env == "development") {
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
-// } else if (env == "test") {
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest";
-// } else {
-//   // mongodb://<dbuser>:<dbpassword>@ds137600.mlab.com:37600/todoapp
-//   process.env.MONGODB_URI =
-//     "mongodb://nikhil:nickMongo18@ds137600.mlab.com:37600/todoapp";
-// }
